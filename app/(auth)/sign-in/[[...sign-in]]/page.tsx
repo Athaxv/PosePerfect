959d2531aebc1141998312a541aef085a3ef4aee
AtharvGaur <atharava112@gmail.com>
2025-03-13 15:11:49 +0530

    Enhance SignIn and SignUp pages with custom styling and layout

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,21 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+    <SignIn
+  appearance={{
+    variables: {
+      colorPrimary: "#2563eb",
+      colorText: "#111827",
+    },
+    elements: {
+      formFieldInput: "bg-gray-200 focus:ring-2 focus:ring-blue-500",
+      button: "bg-indigo-600 text-white hover:bg-indigo-700",
+    },
+  }}
+/>
+</div>
+
+  )
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,21 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className='flex justify-center items-center bg-gray-100 min-h-screen'>
+    <SignUp
+  appearance={{
+    variables: {
+      colorPrimary: "#2563eb",
+      colorText: "#111827",
+    },
+    elements: {
+      formFieldInput: "bg-gray-200 focus:ring-2 focus:ring-blue-500",
+      button: "bg-indigo-600 text-white hover:bg-indigo-700",
+    },
+  }}
+/>
+</div>
+
+  )
 }
